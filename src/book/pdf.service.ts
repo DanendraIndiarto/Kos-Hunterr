0ec1df2fb8ea48741beca9f5e8668319837d9027
Danendra Indiarto <indiartodanendra@gmail.com>
2026-03-11 09:08:06 +0700

    prepare deploy railway

--- src/book/pdf.service.ts
+++ src/book/pdf.service.ts
@@ -61,7 +61,7 @@ export class PdfService {
         ? booking.kos.images[0].file
         : '-';
 
-    doc.image(`KosImage/${image}`, {
+    doc.image(`uploads/${image}`, {
       fit: [250, 200],
       align: 'center',
     });
