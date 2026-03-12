18ba75f5fa8220b3dd4ff9e5ddbfe80e2b453cce
Danendra Indiarto <indiartodanendra@gmail.com>
2026-03-12 09:29:50 +0700

    prepare deploy railway

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+    
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath:
